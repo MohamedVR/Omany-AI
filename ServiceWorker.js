8722089a2bdd572afabc384299b4961ed53c7c66
MohamedVR <mohamed.alaaeldin@5d-vr.com>
2024-09-09 15:59:50 +0300

    NOCACHE

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "DefaultCompany-AI_Speech_Test-0.1";
-const contentToCache = [
-    "Build/Omany-AI.loader.js",
-    "Build/Omany-AI.framework.js",
-    "Build/Omany-AI.data",
-    "Build/Omany-AI.wasm",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
